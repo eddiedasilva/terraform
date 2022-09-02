d7fd85dcf3c745ba55ad82eb1bda956f1f78b6bd
Eddie <eddiedasilva@gmail.com>
2022-09-02 15:00:56 +0100

    var update

--- jenkins-packer-demo/backend.tf
+++ jenkins-packer-demo/backend.tf
@@ -8,7 +8,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-tfstate-eddie"
-    key    = "eu-west-2"
+    #key    = "eu-west-2"
     #region         = "${var.AWS_REGION}"
     #region = "eu-west-2"
   }
